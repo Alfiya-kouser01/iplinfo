38dfa6a9ed25c54c1c0cc72cda38799902fc8721
Alfiya Kouser <alfiyakouser01@gmail.com>
2024-04-22 18:02:17 +0530

    initial commit

---
+++ src/main/java/com/cricket/iplinfo/IplinfoApplication.java
@@ -0,0 +1,13 @@
+package com.cricket.iplinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IplinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IplinfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cricket/iplinfo/IplinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cricket.iplinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IplinfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
