d2d6c66c60e5c3209d5cad7cf0309bca9b0a27f5
Alfiya Kouser <alfiyakouser01@gmail.com>
2024-04-26 20:15:52 +0530

    Changes made in entity,repositiry and service class

--- src/main/java/com/cricket/iplinfo/entity/Match.java
+++ src/main/java/com/cricket/iplinfo/entity/Match.java
@@ -1,25 +1,51 @@
 package com.cricket.iplinfo.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.Date;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Setter
+@Getter
 @Entity
-@Table(name ="STUDENT")
+@Table(name ="cricket")
 public class Match{
     @Id
-    @Column(name = "fingerprint", unique = true)
-    private String fingerprint;
+    @GeneratedValue(strategy=GenerationType.IDENTITY )
+    private int matchId;
 
-    @Column
-    private String name;
+    private String teamHeading;
 
-    @Column
-    private String usn;
+    private String matchNumberVenue;
+
+    private String battingTeam;
+
+    private String battingTeamScore;
+
+    private String bowlTeam;
+
+    private String bowlTeamScore;
+
+    private String liveText;
+
+    private String matchLink;
+
+    private String textComplete;
+
+    @Enumerated
+    private MatchStatuss status;
+
+    private Date date=new Date();
+
+    public void setMatchStatus(){
+        if(textComplete.isBlank()){
+            this.status=MatchStatuss.LIVE;
+        }
+        else{
+            this.status=MatchStatuss.COMPLETED;
+        }
+    }
 }
--- src/main/java/com/cricket/iplinfo/repository/MatchRespository.java
+++ src/main/java/com/cricket/iplinfo/repository/MatchRespository.java
@@ -1,4 +1,10 @@
 package com.cricket.iplinfo.repository;
+import com.cricket.iplinfo.entity.Match;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MatchRespository {
+import java.util.Optional;
+
+public interface MatchRespository extends JpaRepository<Match,Integer> {
+
+    Optional<Match> findByTeamHeading(Integer integer);
 }
